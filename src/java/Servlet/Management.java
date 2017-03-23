a3cecf46d1696cee0ecfe60be07029f6831fa510
Thien <tranhangocthien@gmail.com>
2017-03-23 16:50:43 +0700

    add Filter/LoginFilter.java

--- src/java/Servlet/Management.java
+++ src/java/Servlet/Management.java
@@ -10,8 +10,6 @@ import Model.*;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
