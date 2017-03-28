a303c748c0b4f51a0c215ee57ba76bd625a10698
Thien <tranhangocthien@gmail.com>
2017-03-28 11:07:39 +0700

    commit by Thien

--- src/java/Servlet/ServiceManagement.java
+++ src/java/Servlet/ServiceManagement.java
@@ -13,8 +13,6 @@ import Model.Human;
 import com.google.gson.Gson;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
